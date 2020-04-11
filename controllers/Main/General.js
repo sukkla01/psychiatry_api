e6ac0ff709723d2ed1372ffd22b0bc36e090cea7
sukkla01 <sukkla01@gmail.com>
2020-04-11 21:13:18 +0700

    5

--- controllers/Main/General.js
+++ controllers/Main/General.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 exports.getName = (req, res, next) => {
     let username = req.params.username
 
@@ -29,6 +31,7 @@ exports.create = (req, res, next) => {
         no4: body[0].no4.chioce,
         no5: body[0].no5.chioce,
         no6: body[0].no6.chioce,
+        d_update : moment().format('YYYY-MM-DD') + ' ' + moment().format('HH:mm:ss')
     }
     req.getConnection(function (err, connection) {
         connection.query("insert into quest set ?", post, (err, results) => {
